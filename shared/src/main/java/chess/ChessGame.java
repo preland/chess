d40aaff991ab5cb8fa91436ec6d4b4c7daf638ed
preland <89992615+preland@users.noreply.github.com>
2025-02-04 21:01:21 -0700

    added ChessGame team implementation

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,15 +10,16 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    TeamColor team;
     public ChessGame() {
-
+        team = TeamColor.WHITE;
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return team;
     }
 
     /**
@@ -27,7 +28,8 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.team = team;
+        //throw new RuntimeException("Not implemented");
     }
 
     /**
