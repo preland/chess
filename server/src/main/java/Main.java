f82fd65831a604b956d00ad6f8f7c611863f3e15
preland <89992615+preland@users.noreply.github.com>
2025-03-01 15:32:56 -0700

    create uuid function; implement UserData

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,12 +1,15 @@
 import chess.*;
 import server.Server;
 
-
+import java.util.UUID;
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
         Server server = new Server();
         server.run(8080);
+    }
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
     }
 }
