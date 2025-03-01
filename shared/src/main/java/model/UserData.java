ce1de5109b3057bc5a5ac45a63e65f3ef9561355
preland <89992615+preland@users.noreply.github.com>
2025-03-01 15:36:33 -0700

    fix UserData, implement AuthData and GameData

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,48 +1,5 @@
 package model;
 
-import java.util.Objects;
+public record UserData(String username, String password, String email) {
 
-public class UserData {
-    private final String username, password, email;
-
-    public UserData(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        UserData userData = (UserData) o;
-        return Objects.equals(username, userData.username) && Objects.equals(password, userData.password) && Objects.equals(email, userData.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username, password, email);
-    }
-
-    @Override
-    public String toString() {
-        return "UserData{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
