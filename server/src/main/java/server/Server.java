00bd21eb46a643fb07c7ca70057c5071b4f35492
preland <89992615+preland@users.noreply.github.com>
2025-03-01 16:03:55 -0700

    initial test implementation of endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,8 +11,35 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        //This line initializes the server and can be removed once you have a functioning endpoint
+        Spark.post("/user", (request, response) -> {
+            response.status(200);
+            return "hello";
+        });
+        Spark.post("/session", (request, response) -> {
+            response.status(200);
+            return "jello";
+        });
+        Spark.delete("/session", (request, response) -> {
+            response.status(200);
+            return "bello";
+        });
+        Spark.get("/game", (request, response) -> {
+            response.status(200);
+            return "yello";
+        });
+        Spark.post("/game", (request, response) -> {
+            response.status(200);
+            return "mello";
+        });
+        Spark.put("/game", (request, response) -> {
+            response.status(200);
+            return "rello";
+        });
+        Spark.delete("/db", (request, response) -> {
+            response.status(200);
+            return "tello";
+        });
 
         Spark.awaitInitialization();
         return Spark.port();
