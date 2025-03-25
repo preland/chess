bb7dd0015ddf6069e71a04803e9511c45b29e87e
preland <89992615+preland@users.noreply.github.com>
2025-03-24 20:40:35 -0600

    init handlers

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        //var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         boolean quit = false;
         Scanner scan = new Scanner(System.in);
         //System.out.println("♕ 240 Chess Client: " + piece);
@@ -13,10 +13,28 @@ public class Main {
         while(!quit) {
             System.out.print(">>> ");
             switch (scan.nextLine()) {
+                case "login":
+                    break;
+                case "logout":
+                    break;
+                case "new_game":
+                    break;
+                case "join_game":
+                    break;
                 case "help":
                     break;
+                case "redraw":
+                    break;
+                case "leave":
+                    break;
+                case "quit":
+                    break;
+                case "move":
+                    break;
+                case "show_moves":
+                    break;
                 default:
-                    quit = true;
+                    //help stuff here
                     break;
             }
         }
@@ -25,6 +43,36 @@ public class Main {
         //await user input
         //do action based on input
         //if action is quit, then quit
+    }
+    void handleLogin(){
+        System.out.println("help text");
+    }
+    void handleLogout(){
+        System.out.println("help text");
+    }
+    void handleNewGame(){
+        System.out.println("help text");
+    }
+    void handleJoinGame(){
+        System.out.println("help text");
+    }
+    void handleHelp(){
+        System.out.println("help text");
+    }
+    void handleRedraw(){
+        System.out.println("help text");
+    }
+    void handleLeave(){
+        System.out.println("help text");
+    }
+    void handleQuit(){
+        System.out.println("help text");
+    }
+    void handleMove(){
+        System.out.println("help text");
+    }
+    void handleShowMoves(){
+        System.out.println("help text");
     }
 
 }
